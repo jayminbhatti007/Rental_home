b2f6e5e1eca4b43fb0b74924a5df02ce861e09dc
jayminbhatti007 <tasknest1@gmail.com>
2024-10-02 17:14:26 +0530

    Add files via upload

---
+++ design/db/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "project_prince";
+
+$con = mysqli_connect($host,$username,$pass,$dbname);
+// if($con){
+//     echo "database connected ";
+// }
+// else{
+//     die("connection error:".mysqli_connect_error());
+// }
+
+?>
